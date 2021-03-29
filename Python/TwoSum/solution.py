2357a1ce69a8b098ca0e22d626d93afea494df09
Judah McNicholl <judah@frogparking.com>
2021-03-29 15:37:04 +1300

    TwoSum
    Also fixed up __init.py

--- Python/TwoSum/solution.py
+++ Python/TwoSum/solution.py
@@ -1,10 +1,16 @@
 #Code for question TwoSum in LeetCode
 
 class Solution:
-	def TwoSum():
-		pass
-
-if __name__ == '__main__()':
+	def twoSum(self, nums, target):
+		i = 0
+		while i < len(nums):
+			j = 0
+			while j < len(nums):
+				if i != j:
+					if nums[i] + nums[j] == target:
+						return [i, j]
+				j += 1
+			i += 1
+if __name__ == '__main__':
 	solution = Solution()
-	solution.TwoSum()
-
+	print(solution.twoSum([-1,-2,-3,-4,-5], -8))
--- Python/__init.py
+++ Python/__init.py
@@ -9,6 +9,7 @@ if not os.path.exists(args.question):
 path = os.path.join(args.question, "solution.py")
 
 with open(path, "w") as w:
+    camelCase = f"{args.question[0].lower()}{args.question[1:]}"
     w.writelines(f"#Code for question {args.question} in LeetCode\n\n")
-    w.writelines(f"class Solution:\n\tdef {args.question}():\n\t\tpass\n\n")
-    w.writelines(f"if __name__ == '__main__()':\n\tsolution = Solution()\n\tsolution.{args.question}()\n\n")
+    w.writelines(f"class Solution:\n\tdef {camelCase}(self):\n\t\tpass\n\n")
+    w.writelines(f"if __name__ == '__main__':\n\tsolution = Solution()\n\tsolution.{camelCase}()\n\n")
